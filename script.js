8e56b4255f21469f8d2c0c5b280c53bcf7633b35
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2025-03-07 00:36:53 +0200

    Disable second fourth place coordinate

--- script.js
+++ script.js
@@ -192,10 +192,10 @@ function drawRectangle() {
         return;
     }
 
-    if (x1 > 0 || x2 > 0 || y1 < 0 || y2 < 0) {
-        alert("Координати мають бути у другій системі координат.");
-        return;
-    }
+    // if (x1 > 0 || x2 > 0 || y1 < 0 || y2 < 0) {
+    //     alert("Координати мають бути у другій системі координат.");
+    //     return;
+    // }
 
     let rect = new Rectangle(x1, y1, x2, y2);
     if (isRectangleInArray(rect)) {
